3317b38a0621142c4d029a62f6547a58ebe1291a
falakh <zulfikarfahmi38@gmail.com>
2020-01-16 14:05:34 +0700

    change rdirect url

--- src/util/lineliffhelper.js
+++ src/util/lineliffhelper.js
@@ -20,7 +20,9 @@ class liffHelper {
 
   async getProfile()  {
     await this.init()
-     await liff.login()
+    if(!liff.isLoggedIn()){
+      await liff.login()
+    }
     return new Promise((resolve, reject) => {
       this.init()
         .then(() => {
